2df30b4b3f2a91e1d4b12ad8720fa7836a88a3dd
SANJAY KUMAR <65277126+sanju6890@users.noreply.github.com>
2020-08-30 22:37:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup( name='Voice-Assistant News bot setup',
        version='3.0',
        author='SANJAY KUMAR',
        description='This application only operates through voice-commands.',
-       executables=[Executable(r'C:\Users\sanju\OneDrive\Desktop\IIC contest\news_feeder.py',
-                                icon=r'C:\Users\sanju\OneDrive\Desktop\IIC contest\favicon2.ico',
+       executables=[Executable(r'PATH TO news_feeder.py',
+                                icon=r'PATH TO favicon2.ico',
                                 shortcutName="Voice-Assistant News Bot",shortcutDir='DesktopFolder')]
     )
